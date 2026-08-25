3d63981392077eaf820bd9e29d754ef7b4c86643
KelvinPogo <180973163+KelvinPogo@users.noreply.github.com>
2026-08-24 23:54:06 -0400

    vector integration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
@@ -13,5 +14,13 @@ Here is the question to answer: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-result = chain.invoke({"reviews": [], "question": "What is the best pizza place in town?"})
-print(result)
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
